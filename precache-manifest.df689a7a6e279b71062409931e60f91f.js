bcbe12d3af82a4e2ee758d4d57926ec9afd90e8a
damenor <damenor.develop@gmail.com>
2020-06-24 11:50:43 +0200

    Updates

---
+++ precache-manifest.df689a7a6e279b71062409931e60f91f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22fa27289b2b7993c49c6318d762b01c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "035748cc75254e8608fc",
+    "url": "/static/css/main.2d84f479.chunk.css"
+  },
+  {
+    "revision": "6a643b9e693a9715a281",
+    "url": "/static/js/2.8b0b16f8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8b0b16f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "035748cc75254e8608fc",
+    "url": "/static/js/main.34759467.chunk.js"
+  },
+  {
+    "revision": "e45b57012750ba560b9e",
+    "url": "/static/js/runtime-main.098ada1f.js"
+  },
+  {
+    "revision": "ae427edb9414023b7458031f6c5b5bb5",
+    "url": "/static/media/arrow_down.ae427edb.svg"
+  },
+  {
+    "revision": "d2f4ef6f1433a866894715b1280a14a2",
+    "url": "/static/media/bg_not_found.d2f4ef6f.jpg"
+  },
+  {
+    "revision": "b659403d996f54b0be1f75e66587422a",
+    "url": "/static/media/clouds.b659403d.svg"
+  },
+  {
+    "revision": "6076831051da58ae617452be16a28ced",
+    "url": "/static/media/drizzle.60768310.svg"
+  },
+  {
+    "revision": "e9e99039ad9f8445e9b747f8e0b159ab",
+    "url": "/static/media/loading.e9e99039.gif"
+  },
+  {
+    "revision": "3876ffc76c04bdc564fab4e94f130c35",
+    "url": "/static/media/rain.3876ffc7.svg"
+  },
+  {
+    "revision": "e96a755a78ea3175325446df4ea1a81f",
+    "url": "/static/media/snow.e96a755a.svg"
+  },
+  {
+    "revision": "2f553d2901ca4c91cf54164a77622d08",
+    "url": "/static/media/sun.2f553d29.svg"
+  },
+  {
+    "revision": "a5420362f315ffbf1e46b172710b1f0e",
+    "url": "/static/media/thunderstorm.a5420362.svg"
+  }
+]);
